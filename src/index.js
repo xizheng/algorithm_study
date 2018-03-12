f83308630de42051606d45561bd5ef70e565baba
shixizheng <shixizheng@admaster.com.cn>
2018-03-12 17:29:40 +0800

    palindrome check

--- src/index.js
+++ src/index.js
@@ -2,7 +2,12 @@ const _ = require('lodash')
 const quickSort = require('./quick_sort')
 const bubbleSort = require('./bubble_sort.js')
 const mergeSort = require('./merge_sort')
+const isPalindrome = require('./palindrome_check.js')
 
-let list = _.shuffle(_.range(100))
+let list = _.shuffle(_.range(10000))
+// console.log(mergeSort(list))
 
-console.log(mergeSort(list))
+const strA = _.range(2)
+const strB = _.clone(strA).reverse()
+const isP = Math.random() <= 0.5
+console.log(isP, isPalindrome(strA.join('') + (isP ? strB.join('') : '')))
---
+++ src/palindrome_check.js
@@ -0,0 +1,6 @@
+
+const isPalindrome = (str) => {
+	return str === str.split('').reverse().join('')
+}
+
+module.exports = isPalindrome
